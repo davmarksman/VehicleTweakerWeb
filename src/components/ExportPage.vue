9d11466ff43713b524e41cb0a08d77b81e00cea2
davmarksman <david.ike.mark@gmail.com>
2025-05-04 17:21:50 +0100

    Update ExportPage.vue

--- src/components/ExportPage.vue
+++ src/components/ExportPage.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        <label> The wkvt_settings.lua file should have downloaded to your computer.</label>
+        <label> The wkvt_settings.lua file should have downloaded to your computer. Paste it in the correct directory</label>
+
+        <div>
+            Check here to find the mod directory: <a href="https://www.transportfever2.com/wiki/doku.php?id=gamemanual:gamefilelocations#overview">https://www.transportfever2.com/wiki/doku.php?id=gamemanual:gamefilelocations#overview</a>
+            <br />
+            Then its:
+            <ul>
+              <li>Steam:   /2668633703/res/scripts/wkvt_settings.lua</li>
+              <li>Downloaded: /wk_vehicle_tweaker_1/res/scripts/wkvt_settings.lua</li>
+            </ul>
+            <div class="alert alert-primary" role="alert">
+              <small>C:\Program Files (x86)\Steam\steamapps\workshop\content\1066780\2668633703\res\scripts</small>
+            </div>
+        </div>
+        
         <div class="form-group">
             <label for="textFileArea">
                 You can copy the text from here if the download failed:
